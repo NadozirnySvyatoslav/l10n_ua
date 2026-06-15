103cb3c88f8d281c4e278fb5d4b850b9452f693d
ForcePushAndPray <>
2026-06-15 12:54:03 +0000

    multi-company support implemented for list of orders

--- l10n_ua_hr_documents/models/hr_order.py
+++ l10n_ua_hr_documents/models/hr_order.py
@@ -134,7 +134,7 @@ class HrOrder(models.Model):
         ('cancelled', 'Cancelled'),
     ], string='Status', default='draft', tracking=True)
 
-    company_id = fields.Many2one('res.company', string='Company',
+    company_id = fields.Many2one('res.company', string='Company', required=True, index=True,
                                   default=lambda self: self.env.company)
 
     @api.model_create_multi
